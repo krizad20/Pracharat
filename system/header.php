964a15fd0458edf04fd7865e3528e70e550c70ff
krizad20 <vim_intason@hotmail.com>
2022-04-13 03:30:25 +0700

    test

--- system/header.php
+++ system/header.php
@@ -168,7 +168,7 @@ include('server.php');
 
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Pracharat</a>
+			<a class="navbar-brand" href="#">Pracharat test</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
