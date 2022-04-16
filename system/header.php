57ec26d59a8c55c62dfe181a0fd98d0ea337cafd
krizad20 <vim_intason@hotmail.com>
2022-04-17 00:04:40 +0700

    header

--- system/header.php
+++ system/header.php
@@ -204,9 +204,6 @@ include('server.php');
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<!-- <li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="OnlineCart.php">Home</a>
-					</li> -->
 					<?php
 					if (isset($_SESSION['seller'])) {
 						echo '<li class="nav-item"><a class="nav-link" href="./TabSale.php">หน้าจอขาย</a></li>';
