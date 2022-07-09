fa9a9134456f2642d91732c8651045483817acce
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2022-07-09 14:23:15 +0700

    customer

--- ajax/customer.php
+++ ajax/customer.php
@@ -1,6 +1,6 @@
 <?php
 include(".././system/server.php");
-$sql = "SELECT * FROM `customer` ORDER BY `cID` ASC";
+$sql = "SELECT * FROM `customer` WHERE IsDel = 0 ORDER BY `cID` ASC";
 $res = mysqli_query($conn,$sql);
 
 while($row = mysqli_fetch_array($res,MYSQLI_ASSOC)){
