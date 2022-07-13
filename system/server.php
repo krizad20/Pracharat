c24b5a9c658287c34c7e800ae09c5e3c5602ec61
krizad20 <vim_intason@hotmail.com>
2022-07-13 11:14:58 +0700

    pack product val bp sp

--- system/server.php
+++ system/server.php
@@ -51,16 +51,31 @@ function updateStock($conn, $pID, $newVal, $pBP, $pSP)
 
 function updateForPack($conn, $pID, $newVal, $pBP, $pSP)
 {
-    
+    //Update in Pack
     $sql = "UPDATE product p, packproduct pa 
             SET p.pVal = p.pVal + ((SELECT paPerPAck*$newVal FROM packproduct WHERE paID = '$pID'))
             WHERE p.hasPacked = 1 AND p.pID = (SELECT pID FROM packproduct WHERE paID = '$pID')";
     mysqli_query($conn, $sql);
 
-    $sql = "UPDATE product p, packproduct pa 
-            SET p.pVal = FLOOR((SELECT p.pVal/(pa.paPerPack*1.0) 
-                                FROM product p, packproduct pa 
-                                WHERE p.pID = '$pID'  AND pa.pID = p.pID)) 
-            WHERE p.isPacked = 1 AND p.pID = (SELECT paID FROM packproduct WHERE pID = '$pID')";
-    mysqli_query($conn, $sql);
+    //Update Pack
+    //pID = สินค้าเดี่ยวที่เพิ่มเข้ามา
+    $sql1 = "SELECT pa.paID, FLOOR(p.pVal/(pa.paPerPack*1.0)) as newVal ,pa.paPerPack
+            FROM product p, packproduct pa 
+            WHERE p.pID = '$pID'  AND pa.pID = p.pID";
+    $result1 = mysqli_query($conn, $sql1);
+    while ($row1 = $result1->fetch_assoc()) {
+        $paID = $row1["paID"];
+        $paPerPack = $row1["paPerPack"];
+        $nVal = $row1["newVal"];
+        echo $nVal;
+        $sql2 = "UPDATE product SET pVal = $nVal, pBP = $pBP*$paPerPack, pSP = $pSP*$paPerPack WHERE pID = '$paID' and isPacked = 1";
+        mysqli_query($conn, $sql2);
+    }
+
+    // $sql = "UPDATE product p, packproduct pa 
+    //         SET p.pVal = FLOOR((SELECT p.pVal/(pa.paPerPack*1.0) 
+    //                             FROM product p, packproduct pa 
+    //                             WHERE p.pID = '$pID'  AND pa.pID = p.pID)) 
+    //         WHERE p.isPacked = 1 AND p.pID = (SELECT paID FROM packproduct WHERE pID = '$pID')";
+    // mysqli_query($conn, $sql);
 }
