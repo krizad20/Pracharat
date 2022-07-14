37dc686c9dcc6ad104dc5864f5a172473c038c73
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2022-07-14 10:32:15 +0700

    not clear managePack Modal

--- TabManageProduct.php
+++ TabManageProduct.php
@@ -1323,18 +1323,18 @@ if (!isset($_SESSION['seller']) || $_SESSION['permission'] == "2") {
 
     });
 
-    $('#managePackModal').on('hide.bs.modal', function() {
-      $('#ppaID').val("");
-      $('#ppID').val("");
-      $('#ppBar').val('');
-      $('#ppaPerPacked').val('')
-      $('#ppaBPerOne').val('');
-      $('#ppaID').val('');
-      $('#ppaName').val('');
-      $('#ppaBPerPack').val('');
-      $('#ppaSP').val('');
+    // $('#managePackModal').on('hide.bs.modal', function() {
+    //   $('#ppaID').val("");
+    //   $('#ppID').val("");
+    //   $('#ppBar').val('');
+    //   $('#ppaPerPacked').val('')
+    //   $('#ppaBPerOne').val('');
+    //   $('#ppaID').val('');
+    //   $('#ppaName').val('');
+    //   $('#ppaBPerPack').val('');
+    //   $('#ppaSP').val('');
 
-    });
+    // });
 
 
     function detailBox(mode, type) {
